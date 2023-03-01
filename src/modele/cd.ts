583f2514ad45f85b2a1b9cf79a1b068913ad01e5
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2023-03-01 08:45:25 +0100

    Ajout de cd.ts dans modele contenant les informations a l'affichage

---
+++ src/modele/cd.ts
@@ -0,0 +1,46 @@
+export class CD{
+
+  id!: number;
+  title!: string;
+  artist!: string;
+  genre!: string;
+  price!: number;
+  releaseDate!: Date;
+  cover!: string;
+  quantity!: number;
+  onsale?: boolean;
+
+  constructor(id: number, title: string, artist: string, genre: string, price: number, releaseDate: Date, cover: string, quantity: number, onsale?: boolean) {
+    this.id = id;
+    this.title = title;
+    this.artist = artist;
+    this.genre = genre;
+    this.price = price;
+    this.releaseDate = releaseDate;
+    this.cover = cover;
+    this.quantity = quantity;
+    this.onsale = onsale;
+  }
+
+  toString(): string {
+    return this.title + " - " + this.artist + " - " + this.genre + " - " + this.price + " - " + this.releaseDate + " - " + this.cover + " - " + this.quantity + " - " + this.onsale;
+  }
+
+  toHtml(): string {
+    let html = "<div class='card' style='width: 18rem;'>";
+    html += "<img src='" + this.cover + "' class='card-img-top' alt='...'>";
+    html += "<div class='card-body'>";
+    html += "<h5 class='card-title'>" + this.title + "</h5>";
+    html += "<p class='card-text'>" + this.artist + "</p>";
+    html += "<p class='card-text'>" + this.genre + "</p>";
+    html += "<p class='card-text'>" + this.price + "</p>";
+    html += "<p class='card-text'>" + this.releaseDate + "</p>";
+    html += "<p class='card-text'>" + this.quantity + "</p>";
+    html += "<p class='card-text'>" + this.onsale + "</p>";
+    html += "</div>";
+    html += "</div>";
+    return html;
+
+  }
+
+}
