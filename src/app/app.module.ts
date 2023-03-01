4bcb79bbeef036595b2ad9690c6923d18fa7ff48
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2023-03-01 09:03:48 +0100

    Ajout du header et de la variable dans list-cds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CDComponent } from './cd/cd.component';
 import { ListCDsComponent } from './list-cds/list-cds.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CDComponent,
-    ListCDsComponent
+    ListCDsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/list-cds/list-cds.component.ts
+++ src/app/list-cds/list-cds.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CD } from 'src/modele/cd';
 
 @Component({
   selector: 'app-list-cds',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-cds.component.scss']
 })
 export class ListCDsComponent {
+  listCDs!: CD;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listCDs = new CD("The Dark Side of the Moon", "Pink Floyd", "Rock", 20, new Date(1973, 2, 1), "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png", 20, true);
+
+  }
+
+  toHtml(): string {
+    return this.listCDs.toHtml();
+  }
 
 }
