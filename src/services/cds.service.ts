273bcc822a9886f6fc2d2e77a77b19476c2b2b4e
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2023-03-20 10:30:13 +0100

    Ajout du fichier app-routing

---
+++ src/services/cds.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { CD } from 'src/modele/cd';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CdsService {
+
+  constructor() { }
+
+  getAllCD():CD[]{
+    return [
+      {
+        id: 1,
+        title: "The Dark Side of the Moon",
+        artist: "Pink Floyd",
+        genre: "Rock",
+        price: 15.99,
+        releaseDate: new Date(1973, 2, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 10
+      },
+      {
+        id: 2,
+        title: "The Wall",
+        artist: "Pink Floyd",
+        genre: "Rock",
+        price: 15.99,
+        releaseDate: new Date(1979, 11, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/0/0b/Pink_Floyd_-_The_Wall.jpg",
+        quantity: 40
+      },
+      {
+        id: 3,
+        title: "The Piper at the Gates of Dawn",
+        artist: "Pink Floyd",
+        genre: "Rock",
+        price: 15.99,
+        releaseDate: new Date(1967, 6, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 90
+      },
+      {
+        id: 4,
+        title: "Animals",
+        artist: "Pink Floyd",
+        genre: "Rock",
+        price: 15.99,
+        releaseDate: new Date(1977, 1, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 20
+      },
+      {
+        id: 5,
+        title: "The Freewheelin' Bob Dylan",
+        artist: "Bob Dylan",
+        genre: "Folk",
+        price: 15.99,
+        releaseDate: new Date(1963, 5, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 0
+      },
+      {
+        id: 6,
+        title: "Highway 61 Revisited",
+        artist: "Bob Dylan",
+        genre: "Folk",
+        price: 15.99,
+        releaseDate: new Date(1965, 6, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 200
+      },
+      {
+        id: 7,
+        title: "Blonde on Blonde",
+        artist: "Bob Dylan",
+        genre: "Folk",
+        price: 15.99,
+        releaseDate: new Date(1966, 6, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 255
+      },
+      {
+        id: 8,
+        title: "The Beatles",
+        artist: "The Beatles",
+        genre: "Rock",
+        price: 209.99,
+        releaseDate: new Date(1967, 6, 1),
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        quantity: 300
+      }
+    ];
+  }
+}
