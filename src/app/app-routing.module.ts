946b5938f38fa2b7204f653abb4c00c45eb45b45
Mathis heriveau <mheriveau@iutbayonne.univ-pau.fr>
2023-03-22 17:22:57 +0100

    Ajout et reglage du bug home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {ListCDsComponent} from "./list-cds/list-cds.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Route[] = [
+    { path: '', component: HomeComponent },
+    { path: 'listcds', component: ListCDsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CDComponent } from './cd/cd.component';
 import { ListCDsComponent } from './list-cds/list-cds.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CDComponent,
     ListCDsComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
